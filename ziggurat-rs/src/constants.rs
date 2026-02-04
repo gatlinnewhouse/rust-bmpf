c4ef901c1071cf3670bf6246c130b3b44e0930de
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2026-02-04 12:29:00 -0800

    remove unused ziggurat constants from lib

--- ziggurat-rs/src/constants.rs
+++ ziggurat-rs/src/constants.rs
@@ -1,11 +1,8 @@
 //! Constants for the Ziggurat algorithm
 
-pub const ZIGGURAT_TABLE_SIZE: usize = 256;
-
 // Normal distribution constants
 pub const ZIGGURAT_NOR_R: f64 = 3.654_152_885_361_008_8;
 pub const ZIGGURAT_NOR_INV_R: f64 = 0.273_661_237_329_758_28;
 
 // Exponential distribution constants
 pub const ZIGGURAT_EXP_R: f64 = 7.697_117_470_131_049_72;
-pub const ZIGGURAT_EXP_INV_R: f64 = 0.129_918_765_548_341_586;
